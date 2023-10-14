265189331aa32a10132d0f1bf40d6a5d1bb9ddd9
Veer <veerjhabak47@gmail.com>
2023-10-14 19:57:47 +0530

    first commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function Nav() {
   return (
