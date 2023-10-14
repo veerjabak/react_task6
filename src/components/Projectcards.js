f5288fb1d5cd42f7d20c8856a17c575588c30d71
Divij <divijgamer4@gmail.com>
2023-10-14 20:48:26 +0530

    first commit

--- src/components/Projectcards.js
+++ src/components/Projectcards.js
@@ -7,12 +7,12 @@ const Projectcards = ({ name, description, techStack, image }) => {
         src={image}
         className="card-img-top"
         alt={name}
-        style={{ width: "80px", height: "100px" }}
+        style={{ width: "100%", height: "100px" }}
       />
       <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text">Description: {description}</p>
-        <p className="card-text">Tech Stack: {techStack}</p>
+        <h5 className="card-title text-dark">{name}</h5>
+        <p className="card-text text-dark">Description: {description}</p>
+        <p className="card-text text-dark">Tech Stack: {techStack}</p>
       </div>
     </div>
   );
